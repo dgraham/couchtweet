515489a2201ef616837235ea98c377f27eee87c0
David Graham <david.malcom.graham@gmail.com>
2012-07-04 12:12:40 -0600

    Allow a user to delete their own tweets.

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,13 +1,21 @@
 class StatusController < ApplicationController
-  before_filter :authenticate, :only => :create
+  before_filter :authenticate, :except => :show
 
   def show
-    @tweet = Tweet.get(params[:id])
+    @tweet = Tweet.get!(params[:id])
     @user = @tweet.user
   end
 
   def create
     current_user.tweet(params[:status], 'web')
+    redirect_to root_path
+  end
+
+  def destroy
+    tweet = Tweet.get!(params[:id])
+    if tweet && tweet.author?(current_user)
+      tweet.destroy
+    end
     redirect_to root_path
   end
 end
