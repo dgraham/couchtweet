6c729558abf940ac05b1e16e55d1e6a9f0669efd
David Graham <david.malcom.graham@gmail.com>
2012-07-08 15:05:04 -0600

    Add follow/unfollow links to user profile page.

--- app/views/users/show.rb
+++ app/views/users/show.rb
@@ -5,6 +5,21 @@ module Views
       def page_title
         "%s (%s) on CouchTweet" % [user.name, user.id]
       end
+
+      # Generate a link to follow/unfollow the user.
+      #
+      # Returns an HTML `a` tag String to the follow or unfollow actions
+      # depending on whether the current user is already following the user or
+      # not.
+      def link_to_follow
+        return if !current_user || current_user == user
+        if current_user.following?(user)
+          url = user_following_path(user, current_user)
+          link_to('Unfollow', url, :method => :delete, :class => 'unfollow')
+        else
+          link_to('Follow', following_path, :method => :create, :class => 'follow')
+        end
+      end
     end
   end
 end
