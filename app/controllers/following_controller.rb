36d1e8792bd77a05ea0e9c8b751352488b2bba9a
David Graham <david.malcom.graham@gmail.com>
2012-07-08 15:04:19 -0600

    Support follow and unfollow actions.

--- app/controllers/following_controller.rb
+++ app/controllers/following_controller.rb
@@ -1,5 +1,19 @@
 class FollowingController < ApplicationController
+  before_filter :authenticate, :except => :index
+
   def index
-    @user = User.get(params[:user_id])
+    @user = User.get!(params[:user_id])
+  end
+
+  def create
+    user = User.get!(params[:user_id])
+    current_user.follow(user)
+    redirect_to user_path(user)
+  end
+
+  def destroy
+    user = User.get!(params[:user_id])
+    current_user.unfollow(user)
+    redirect_to user_path(user)
   end
 end
