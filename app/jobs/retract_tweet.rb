67f50e16afb4eb63cfc193ed30fe1f96f65ef1f2
David Graham <david.malcom.graham@gmail.com>
2012-07-08 15:37:15 -0600

    Delete favorites for a deleted tweet.

--- app/jobs/retract_tweet.rb
+++ app/jobs/retract_tweet.rb
@@ -9,6 +9,12 @@ class RetractTweet
   @queue = :retract_tweets
 
   def self.perform(tweet_id)
+    # you never liked this tweet
+    Favorite.by_tweet_id.key(tweet_id).each do |favorite|
+      favorite.destroy
+    end
+
+    # you never saw this tweet on your timeline
     TimelineEntry.by_tweet_id.key(tweet_id).each do |entry|
       entry.destroy
     end
